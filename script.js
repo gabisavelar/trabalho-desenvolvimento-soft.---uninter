85d742f368c3c544f3eebd199221e3985b12d522
gabisavelar <gabriellesavelar@gmail.com>
2025-07-06 19:03:21 -0300

    Upload de arquivos
    Upload de arquivos de trabalho universitario de um site sobre interesses pessoais, gostos e curiosidades sobre mim feitas em html, css e javascript.

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('msgBtn').addEventListener('click', function() {
+    const mensagemDiv = document.getElementById('mensagem');
+    mensagemDiv.textContent = 'Seja bem-vindo(a)! Continue explorando o mundo da tecnologia com entusiasmo!';
+    mensagemDiv.style.marginTop = '10px';
+    mensagemDiv.style.fontWeight = 'bold';
+    mensagemDiv.style.color = '#2a6496';
+});
